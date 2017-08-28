4610b8e51bd6ad3fc4077b997eb50314d1d764ff
Paulo-Sergio <paulofranca.contato@gmail.com>
2017-08-27 22:19:00 -0300

    implementando o componente tentativas.component.ts do jogo

--- app1/src/app/painel/painel.component.ts
+++ app1/src/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Frase } from '../shared/frase.model'
 import { FRASES } from './frases-mock'
@@ -21,6 +21,8 @@ export class PainelComponent implements OnInit {
 
   public tentativas: number = 3
 
+  @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter()
+
   constructor() {
     this.atualizaRodada()
   }
@@ -37,15 +39,20 @@ export class PainelComponent implements OnInit {
     if (this.rodadaFrase.frasePtBr == this.resposta) {
       // trocar pergunta da rodada
       this.rodada++
-      this.atualizaRodada()
 
       // barra de progresso
       this.progresso = this.progresso + (100 / this.frases.length)
+
+      if (this.rodada === 4) {
+        this.encerrarJogo.emit('vitoria')
+      }
+
+      this.atualizaRodada()
     } else {
       // diminiur a variavel tentativas
       this.tentativas--;
       if (this.tentativas === -1) {
-        alert('acabou as tentativas')
+        this.encerrarJogo.emit('derrota')
       }
     }
   }
--- app1/src/app/tentativas/tentativas.component.ts
+++ app1/src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 
 import { Coracao } from '../shared/coracao.model'
 
@@ -7,7 +7,7 @@ import { Coracao } from '../shared/coracao.model'
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnInit, OnChanges {
 
   @Input() public tentativas: number
 
@@ -21,8 +21,15 @@ export class TentativasComponent implements OnInit {
     new Coracao(true)
   ]
 
-  ngOnInit() {
+  ngOnChanges(): void {
+    if (this.tentativas !== this.coracoes.length) {
+      let indice = this.coracoes.length - this.tentativas
+      this.coracoes[indice - 1].cheio = false
+    }
     console.log('Tentativas recebidas do painel: ' + this.tentativas)
+  }
+
+  ngOnInit() {
   }
 
 }
