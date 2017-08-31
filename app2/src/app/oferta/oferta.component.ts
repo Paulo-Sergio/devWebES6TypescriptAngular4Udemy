8726017719c38c07b16a51d8bba1994037b858e7
Paulo-Sergio <paulofranca.contato@gmail.com>
2017-08-31 11:46:27 -0300

    recuperando parametros da rota e trazendo informações de detalhe de uma oferta

--- app2/src/app/oferta/oferta.component.ts
+++ app2/src/app/oferta/oferta.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
+import { OfertasService } from '../ofertas.service'
+import { Oferta } from "../shared/oferta.model";
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers: [OfertasService]
 })
 export class OfertaComponent implements OnInit {
 
-  private route: ActivatedRoute
+  public oferta: Oferta
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private ofertasServices: OfertasService) { }
 
   ngOnInit() {
+
+    let id = this.route.snapshot.params['id']
+    this.ofertasServices.getOfertaPorId(id)
+      .then((oferta: Oferta) => {
+        console.log(oferta)
+        this.oferta = oferta
+      })
   }
 
 }
