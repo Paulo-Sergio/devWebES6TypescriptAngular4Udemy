5ede6d21037fb976be6c2989d9fe33df999c5f9b
Paulo-Sergio <paulofranca.contato@gmail.com>
2018-01-19 19:34:09 -0300

    realizando envio de Pedido via POST para a API (json-server)

---
+++ app2/src/app/ordem-compra.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { Http, RequestOptions, Headers, Response } from '@angular/http';
+import { Pedido } from "app/shared/pedido.model";
+import { Observable } from "rxjs/Observable";
+import { URL_API } from "app/app.api";
+
+@Injectable()
+export class OrdemCompraService {
+
+    constructor(private http: Http) { }
+
+    public efetivarCompra(pedido: Pedido): Observable<any> {
+        let headers: Headers = new Headers()
+        headers.append('Content-type', 'application/json')
+
+        return this.http.post(
+            `${URL_API}/pedidos`, 
+            JSON.stringify(pedido),
+            new RequestOptions({headers: headers})
+        ).map((resposta: Response) => {
+            console.log(resposta.json())
+        })
+    }
+}
