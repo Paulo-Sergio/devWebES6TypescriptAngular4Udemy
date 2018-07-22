176c30750c3dd00c8092970629e8fbbff8995557
Paulo França <paulofranca.contato@gmail.com>
2018-07-22 14:09:23 -0300

    criando animações com angular/animations - animations, triggers, states e styles - transition e animate - implementando logica para o banner - renderização com component de login ou cadastro (ngIf)

--- app3/src/app/acesso/banner/banner.component.ts
+++ app3/src/app/acesso/banner/banner.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
+import { Imagem } from './imagem.model';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      })),
+      transition('escondido => visivel', animate('1s ease-in')),
+      transition('visivel => escondido', animate('1s ease-in'))
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
+
+  public estado: string = 'visivel'
+
+  public imagens: Imagem[] = [
+    { estado: 'visivel', url: '/assets/banner-acesso/img_1.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_2.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_3.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_4.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_5.png' },
+  ]
 
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => this.logicaRotacao(), 3000)
   }
+
+  public logicaRotacao(): void {
+    console.log(this.imagens)
+
+    let aux: number
+  
+    // ocultar imagem
+    for (let i: number = 0; i < 5; i++) {
+      if (this.imagens[i].estado === 'visivel') {
+        this.imagens[i].estado = 'escondido'
+        aux = (i === 4) ? 0 : ++i
+        break
+      }
+    }
+
+    // exibir a proxima imagem
+    this.imagens[aux].estado = 'visivel'
+
+    setTimeout(() => this.logicaRotacao(), 3000)
+  }
+
+  /**
+  public toggleEstado(): void {
+    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel'
+  }
+   */
 
 }
