76da73b676bddace719897c12c914cd2a08587fb
Paulo França <paulofranca.contato@gmail.com>
2018-07-22 22:16:02 -0300

    instalando sdk do firebase no projeto; cadastrando usuarios no firebase;

--- app3/src/app/app.component.ts
+++ app3/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+
+  ngOnInit(): void {
+    
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyDVwlMXH0S0Tfst5XPR9Dwzbd9bBhB35gM",
+      authDomain: "instagram-clone-9662e.firebaseapp.com",
+      databaseURL: "https://instagram-clone-9662e.firebaseio.com",
+      projectId: "instagram-clone-9662e",
+      storageBucket: "instagram-clone-9662e.appspot.com",
+      messagingSenderId: "139673862437"
+    }
+
+    firebase.initializeApp(config)
+  }
+
 }
--- app3/src/app/auth.service.ts
+++ app3/src/app/auth.service.ts
@@ -1,9 +1,27 @@
-import { Usuario } from "./acesso/usuario.model";
+import { Usuario } from "./acesso/usuario.model"
+import * as firebase from 'firebase'
 
 export class AuthService {
 
   public cadastrarUsuario(usuario: Usuario): void {
     console.log('auth sevice')
+
+    firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
+      .then((resp: any) => {
+        // remover atributo senha do obj usuario (não vou armazenar a senha)
+        delete usuario.senha
+
+        /** 
+         * btoa() criptografia para base64
+         * atob() descriptografia da base64
+        */
+        // registrando dados complementares do usuario no path email na base64
+        firebase.database().ref(`usuario_detalhe/${btoa(usuario.email)}`)
+          .set(usuario)
+      })
+      .catch((error: Error) => {
+        console.log(error)
+      })
   }
 
 }
