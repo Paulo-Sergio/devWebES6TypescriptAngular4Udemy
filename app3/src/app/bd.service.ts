bbc42e20de792b5d9ea3d8bdd1d783ef4563220c
Paulo França <paulofranca.contato@gmail.com>
2018-07-27 23:49:31 -0300

    controlando o progresso do upload

--- app3/src/app/bd.service.ts
+++ app3/src/app/bd.service.ts
@@ -11,6 +11,16 @@ export class BDService {
     firebase.storage().ref()
       .child(`imagens/${nomeImagem}`)
       .put(publicacao.imagem)
+      .on(firebase.storage.TaskEvent.STATE_CHANGED,
+        // acompanhamento do processo de upload
+        (snapshot: any) => {
+          console.log(snapshot)
+        }, (error) => {
+          console.log(error)
+        }, () => {
+          console.log('upload completo')
+        }
+    )
 
     /*firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
       .push({ titulo: publicacao.titulo })*/
